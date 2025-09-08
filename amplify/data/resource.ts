05a12a3540d66383f8380f1eae4baeadeb5b43bc
Stefan Axelsson <26811776+saxelsso@users.noreply.github.com>
2025-09-08 20:26:10 +0200

    directory housekeeping

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,7 +1,7 @@
 import { type ClientSchema, a, defineData, defineFunction } from "@aws-amplify/backend";
 
 const echoServiceFn = defineFunction({
-    entry: '../functions/echo-service/handler.ts',
+    entry: '../functions/echo-service.ts',
 
 });
 
@@ -44,31 +44,3 @@ export const data = defineData({
     functions: { echoServiceFn },
 });
 
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
--- amplify/functions/echo-service.ts
+++ amplify/functions/echo-service.ts
@@ -1,9 +1,8 @@
-import type { Handler } from 'aws-lambda';
 import { Amplify } from "aws-amplify";
 import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
 import { generateClient } from "aws-amplify/data";
 import { env } from "$amplify/env/handler";
-import { Schema } from '../../../amplify/data/resource';
+import { Schema } from '../data/resource';
  
 const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env);
 
@@ -18,6 +17,6 @@ export const handler: Schema["echoService"]["functionHandler"] = async (event) =
     const todos = await client.models.Todo.list() // use this Data client for CRUDL requests
     console.log("Todos in the database:", todos);
     console.log("Echoing:", echoString + env.AWS_REGION);
-    
+
     return `Hello, ${echoString}! Number of todos: ${todos.data.length}`;
 }
