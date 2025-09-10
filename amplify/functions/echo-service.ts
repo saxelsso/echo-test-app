607ea510430ecbb8136bb3ba6d2480c533d0c950
Stefan Axelsson <26811776+saxelsso@users.noreply.github.com>
2025-09-10 10:31:28 +0200

    add user listing via Cognito in echoService

--- amplify/functions/echo-service.ts
+++ amplify/functions/echo-service.ts
@@ -1,6 +1,11 @@
 import { Amplify } from "aws-amplify";
 import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
 import { generateClient } from "aws-amplify/data";
+import {
+    CognitoIdentityProviderClient,
+    AdminAddUserToGroupCommand,
+    ListUsersCommand,
+} from '@aws-sdk/client-cognito-identity-provider';
 import { env } from "$amplify/env/echo-service";
 import { Schema } from '../data/resource';
  
@@ -10,13 +15,27 @@ Amplify.configure(resourceConfig, libraryOptions);
 
 const client = generateClient<Schema>();
 
+const authClient = new CognitoIdentityProviderClient();
+
 export const handler: Schema["echoService"]["functionHandler"] = async (event) => {
     // arguments typed from `.arguments()`
-    const { echoString } = event.arguments
-
+    const { echoString } = event.arguments;
     const todos = await client.models.Todo.list() // use this Data client for CRUDL requests
     console.log("Todos in the database:", todos);
     console.log("Echoing:", echoString + env.AWS_REGION);
+
+    const input = {
+        AttributesToGet: [
+            "email",
+            "sub"
+        ],
+        UserPoolId: env.AMPLIFY_AUTH_USERPOOL_ID
+    };
+
+    const command = new ListUsersCommand(input);
+    const response = await authClient.send(command);
+
+    console.log(response);
 
     return `Hello, ${echoString}! Number of todos: ${todos.data.length}`;
 }
